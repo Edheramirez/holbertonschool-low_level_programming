ded61d58e8a5ea588888401be1c214e22a668dae
Edheramirez <edher.ramirez@outlook.es>
2021-02-04 23:57:49 -0500

    print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entrty
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n >= 'z'; ++n)
+	{
+		while (n != 'q' && n != 'e')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
