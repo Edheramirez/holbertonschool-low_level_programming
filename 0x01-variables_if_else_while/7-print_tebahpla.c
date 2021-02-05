9906e1a423da91142c90e8a8029f4ee3301cec47
Edheramirez <edher.ramirez@outlook.es>
2021-02-05 00:40:18 -0500

    print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entrty
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+
+}
