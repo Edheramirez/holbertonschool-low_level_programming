a209cd969740f14d2e61839061da34fb55af2b53
Edheramirez <edher.ramirez@outlook.es>
2021-02-04 23:11:59 -0500

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entrty
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		putchar(al);
+	putchar('\n');
+	return (0);
+}
