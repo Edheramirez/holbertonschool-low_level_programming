3f0ba9e6fcaacae2f7cc7f03839c29e1aac5b86b
Edheramirez <edher.ramirez@outlook.es>
2021-02-05 00:19:43 -0500

    print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entrty
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
