da63f383dc06be2843b7987ceed4785d4b11428f
Edheramirez <edher.ramirez@outlook.es>
2021-02-07 17:39:06 -0500

    print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entrty
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
+		if (n == 99)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
