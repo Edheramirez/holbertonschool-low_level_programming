f07b66fc883ee2f506c74f1c3d13a8df5ec31bea
Edheramirez <edher.ramirez@outlook.es>
2021-02-08 23:09:31 -0500

    first_files

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+/* _putchar - Print on screen*/
+int _putchar(char c);
+
+/* print_alphabet - print alphabet on screen*/
+void print_alphabet(void);
+
+/* print_alphabetx10 - Print alphabetx10 on screen*/
+void print_alphabetx10(void);
+
+/* _islower - */
+int _islower(int c);
