a699426bdd5ab2e0f5ebff0ab04b6a5905b46189
Edheramirez <edher.ramirez@outlook.es>
2021-02-09 20:30:59 -0500

    update_header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,8 +4,11 @@ int _putchar(char c);
 /* print_alphabet - print alphabet on screen*/
 void print_alphabet(void);
 
-/* print_alphabetx10 - Print alphabetx10 on screen*/
-void print_alphabetx10(void);
+/* print_alphabet_x10 - Print alphabet x10 on screen*/
+void print_alphabet_x10(void);
 
-/* _islower - */
+/* _islower - checks for a lower-case character */
 int _islower(int c);
+
+/* _isalpha - checks for passed character is alphabetic */
+int _isalpha(int c);
