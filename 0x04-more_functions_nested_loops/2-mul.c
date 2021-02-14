87086448802570c07513f13cf7844158ed665f7b
Edheramirez <edher.ramirez@outlook.es>
2021-02-14 18:31:07 -0500

    mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two int
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * return: int result multiplies
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
