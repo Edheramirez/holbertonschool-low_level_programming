7c9ec208d57ae149f1bf9bedf121d6f4f246d7d9
Edheramirez <edher.ramirez@outlook.es>
2021-02-14 17:48:59 -0500

    0-1-README

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ *@c: integer
+ *
+ * return: 1 if is a uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* int _isupper - checks for uppercase character */
+int _isupper(int c);
+
+/* _isdigit - checks for a digit */
+int _isdigit(int c);
+
+#endif
