c2b7f888b4fa764ebf828594160230e9512471ba
Edheramirez <edher.ramirez@outlook.es>
2021-02-16 13:06:37 -0500

    update_header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,4 +4,7 @@
 /* takes a pointer an int as parameter and update the value */
 void reset_to_98(int *n);
 
+/* swap_int - swaps the value of two integers */
+void swap_int(int *a, int *b);
+
 #endif
