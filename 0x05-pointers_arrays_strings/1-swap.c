257a4ef166579e3608b2af0e79cebc535c1227cb
Edheramirez <edher.ramirez@outlook.es>
2021-02-16 13:52:39 -0500

    swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Sucess)
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
