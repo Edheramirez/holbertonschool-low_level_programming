273c7a01175990f507b61e19e7b8728002807a88
Edheramirez <edher.ramirez@outlook.es>
2021-02-16 00:09:14 -0500

    reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - upload the value of points to 98
+ *
+ * @n: pointer to update
+ *
+ * Return - always 0 (Succes)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
