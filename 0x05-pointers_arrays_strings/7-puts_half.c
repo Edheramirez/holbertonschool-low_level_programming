5fd3fe2ecf0c82863584c094225d5cb5d5301c56
Edheramirez <edher.ramirez@outlook.es>
2021-02-17 13:01:49 -0500

    puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string and new line
+ *
+ * @str: adress of a string
+ *
+ * Return: half of a string
+*/
+void puts_half(char *str)
+{
+	int i;
+	int m;
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	if (l % 2 == 0)
+		m = l / 2;
+	else
+		m = (l - 1) / 2;
+
+	for (i = m; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
