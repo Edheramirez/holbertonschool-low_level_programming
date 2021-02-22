ba8e202573f577d4c10a29de9427f7900f5aa246
Edheramirez <edher.ramirez@outlook.es>
2021-02-22 17:45:29 -0500

    update header

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /* *_memset - fills memory with a constant byte */
 char *_memset(char *s, char b, unsigned int n);
 
+/* _strspn - gets the length of a prefix substring and find the coincidences */
+unsigned int _strspn(char *s, char *accept);
+
 #endif
