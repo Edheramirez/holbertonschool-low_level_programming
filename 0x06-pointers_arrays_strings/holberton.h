f3f4f4b9f3d639cf629f099e6a7663b20c846abd
Edheramirez <edher.ramirez@outlook.es>
2021-02-21 22:57:59 -0500

    update header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -16,4 +16,7 @@ char *_strncpy(char *dest, char *src, int n);
 /* _strcmp - compares two strings */
 int _strcmp(char *s1, char *s2);
 
+/* reverse_array - reverses the content of an array of integers */
+void reverse_array(int *a, int n);
+
 #endif
