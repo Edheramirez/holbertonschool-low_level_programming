cffc82b829aeac428c3eab817cb2450c90ca7203
Edheramirez <edher.ramirez@outlook.es>
2021-02-22 11:58:16 -0500

    start new proyect

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: adress
+ * @b: character
+ * @n: unsigned integer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
