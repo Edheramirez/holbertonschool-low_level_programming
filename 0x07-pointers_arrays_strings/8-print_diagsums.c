a1865fef9579a0aaa412d88826ae109656339448
Edheramirez <edher.ramirez@outlook.es>
2021-02-23 00:13:10 -0500

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsum - printsthesum of the two diagonals square matrix of integers
+ * @a: adress of array of integers
+ * @size: length of matriz
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int iterador;
+	int count = 0;
+	int count2 = 0;
+
+	for (iterador = 0; iterador < size; iterador++)
+	{
+		i = iterador % size;
+		count += a[i + (i * size)];
+		count2 += a[i + ((size - 1 - i) * size)];
+	}
+	printf("%d, %d\n", count, count2);
+}
