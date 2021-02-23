fc30e86946192ac696e25a84a9e0208a24fa0cd7
Edheramirez <edher.ramirez@outlook.es>
2021-02-23 00:33:28 -0500

    set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: adress to a first array
+ * @to: adress to a second array
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
