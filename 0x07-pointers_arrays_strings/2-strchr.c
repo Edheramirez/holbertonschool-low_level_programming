6241cdb3f1f392874c6603d8ef35b84c54798377
Edheramirez <edher.ramirez@outlook.es>
2021-02-23 11:15:30 -0500

    strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: adress to a string
+ * @c: character to copies
+ * Return: pointer to the first occurrence of the character c in the string s
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++, s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return ('\0');
+}
