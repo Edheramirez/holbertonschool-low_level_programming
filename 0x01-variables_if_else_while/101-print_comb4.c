c31ed852b781dfedd1d3fe280aa80c6a4b4afd60
Edheramirez <edher.ramirez@outlook.es>
2021-02-28 14:07:59 -0500

    0-main and holberton.h

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,25 +10,29 @@ int main(void)
 	int a;
 	int b;
 	int c;
-	int num;
 
-	for (num = 0; num <= 999; num++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		a = (a / 10 < a % 10);
-		b = (b / 10 < b % 10);
-		c = (c / 10 < c % 10);
-		if ((a > b) && (b > c))
+		for (b = '0'; b <= '9'; b++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(c + '0');
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
 
-		/*if ( < 789)*/
-
-			putchar(',');
-			putchar(' ');
+					if (a != '7' || b != '8' || c != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
