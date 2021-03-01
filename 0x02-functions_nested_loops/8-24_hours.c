1a4c0642366d49088b64d6066e957bf40c0eacf5
Edheramirez <edher.ramirez@outlook.es>
2021-02-28 20:58:15 -0500

    fix 24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,40 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - the longest day of my life
  *
- * Return: Always 0 (Succes)
+ * Return: 0
  */
 void jack_bauer(void)
 {
-	int n;
+	int d;
 
-	for ()
+	for (d = 0; d <= 2; d++)
 	{
-		putchar();
-		putchar();
+		int c;
+
+		for (c = 00; c <= 9; c++)
+		{
+			int b;
+
+			for (b = 0; b <= 5; b++)
+			{
+				int a;
+
+				for (a = 0; a <= 9; a++)
+				{
+					_putchar(d + '0');
+					_putchar(c + '0');
+					_putchar(':');
+					_putchar(b + '0');
+					_putchar(a + '0');
+					_putchar('\n');
+					if (d == 2 && c == 3 && b == 5 && a == 9)
+					{
+						return;
+					}
+				}
+			}
+		}
 	}
-	if ()
-	{
-		putchar();
-		putchar();
-	}
-	putchar('\n');
-	return (0);
 }
