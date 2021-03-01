f65c2696a85ec99a3ace7bca8937c3a996055e50
Edheramirez <edher.ramirez@outlook.es>
2021-03-01 00:19:53 -0500

    mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of arguments.
+ * @argv: Numbers to multiply.
+ * Return: If no arguments prints Error and Returns 1.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", ((atoi(argv[1])) * (atoi(argv[2]))));
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
