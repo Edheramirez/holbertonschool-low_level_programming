29da137ea010e47688236bae492651c0100581b0
Edheramirez <edher.ramirez@outlook.es>
2021-02-28 21:42:50 -0500

    update header, 10-main.c and 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @n1: first integer
+ * @n2: second integer
+ * Return: the result
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
