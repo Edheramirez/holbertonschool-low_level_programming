0165257af2472fe74521e5f8a6c8d7004d2eac2b
Edheramirez <edher.ramirez@outlook.es>
2021-02-28 20:48:13 -0500

    fix sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,25 +5,26 @@
  *
  *@n: integer
  *
- * Return: Always 0 (Succes)
+ * Return: 1 is greater than 0, 0 is zero, -1 is lesser than 0.
 */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 
 	if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 
 	if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
+	return (0);
 }
