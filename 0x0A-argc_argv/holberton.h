7005e09582e483e8237ade4015c6eaeebd535ce5
Edheramirez <edher.ramirez@outlook.es>
2021-03-01 00:09:21 -0500

    start new proyect

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/* _putchar - writes the character c to stdout */
+int _putchar(char c);
+
+/* main - argc argv */
+int main(int argc, char* argv[]);
+
+#endif
