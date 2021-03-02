9e810df990c693bc20d7a77e08821c204da9c1fa
Edheramirez <edher.ramirez@outlook.es>
2021-03-02 15:11:51 -0500

    start new proyect malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array whit the first char is especific
+ * @size: length of an array
+ * @c: is a character to initializate
+ * Return: NULL if size is 0 or a pointer to array
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+	array = (char *)malloc(sizeof(int) * size);
+
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+		return (*array);
+	}
+	return ('\0');
+}
