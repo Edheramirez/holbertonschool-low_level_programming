75cf886cabcd226f159ea354675b7c816306dd96
Edheramirez <edher.ramirez@outlook.es>
2021-03-02 16:25:29 -0500

    update holberton.h and add 1main.c

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,7 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* creates an array of chars and initializes whit special char */
+/* create_array - creates an array of chars and initializes whit special char */
 char *create_array(unsigned int size, char c);
+
+/* _strdup - returns a pointer to anew allocated space in memory  */
+char *_strdup(char *str);
 
 #endif
