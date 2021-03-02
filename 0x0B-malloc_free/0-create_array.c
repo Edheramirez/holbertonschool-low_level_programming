0e90bbe477fa0d7811d77c6f0d4a8e9211ed337f
Edheramirez <edher.ramirez@outlook.es>
2021-03-02 16:17:27 -0500

    fix return NULL 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,16 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	array = malloc(sizeof(char) * size);
-	if (size != 0)
-	{
-		for (i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		{
 			array[i] = c;
 		}
 		return (array);
-	}
-	return ('\0');
+
+	if (array == NULL)
+		return ('\0');
+
+	if (!size)
+		return ('\0');
 }
