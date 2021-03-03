5702efb231657e22a198f11fdcd4674b2415f615
Edheramirez <edher.ramirez@outlook.es>
2021-03-03 16:25:21 -0500

    fix coments and undeclared variable alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,13 +3,14 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of int
- * @width:
- * @heigth:
+ * @width: second malloc give rows
+ * @height: first malloc give columns
  * Return: Pointer 2 dimensional array
 */
 int **alloc_grid(int width, int height)
 {
 	int i;
+	int j;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -34,7 +35,7 @@ int **alloc_grid(int width, int height)
 				free(grid[i]);
 			}
 			free(grid);
-			return(NULL);
+			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
