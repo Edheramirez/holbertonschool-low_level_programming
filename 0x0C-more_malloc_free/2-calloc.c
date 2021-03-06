1a3bc39fb0daf50e1e4d4192268d8849f748fd13
Edheramirez <edher.ramirez@outlook.es>
2021-03-05 23:28:11 -0500

    cast at the end

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	buffer = malloc(size * nmemb);
+	buffer = malloc(size * nmemb + 1);
 	if (buffer == NULL)
 		return (NULL);
 
