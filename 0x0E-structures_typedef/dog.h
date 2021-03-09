8f494b1f521341a07860f7abbbb3f799eee55e46
Edheramirez <edher.ramirez@outlook.es>
2021-03-08 19:53:25 -0500

    star new proyect structure and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef TYPEDEF_DOG_H
+#define TYPEDEF_DOG_H
+
+/**
+ * struct dog - information from a pet
+ * @name: It´s a name from a pet.
+ * @age: It´s an age from a pet.
+ * @owner: It´s a name for the owner of a pet.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
