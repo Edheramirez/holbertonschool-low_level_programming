dbea92cca4a962e60b746c1a7dbbfa81f7c6a87c
Edheramirez <edher.ramirez@outlook.es>
2021-03-08 20:23:51 -0500

    update header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* init_dog - function that initialize a variable of type struct dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
