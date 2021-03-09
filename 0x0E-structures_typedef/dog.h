fae6ca1f9aef601095f05bfb66e279e35dd711b3
Edheramirez <edher.ramirez@outlook.es>
2021-03-08 22:49:59 -0500

    update header typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,19 +7,18 @@
  * @age: It´s an age from a pet.
  * @owner: It´s a name for the owner of a pet.
 */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /* init_dog - function that initialize a variable of type struct dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /* print_dog - function that prints a struct dog */
 void print_dog(struct dog *d);
-
-typedef dog dog_t
 
 #endif
