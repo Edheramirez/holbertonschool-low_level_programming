1cc8f14a6305a119ab89fec5bd19ee7f22040045
Edheramirez <edher.ramirez@outlook.es>
2021-03-11 18:49:56 -0500

    sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters.
+ * @n: parameters to sum.
+ *
+ * Return: sum of parameters.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list r;
+	unsigned int i, sum = 0;
+
+	va_start(r, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(r, int);
+
+	va_end(r);
+	return (sum);
+}
