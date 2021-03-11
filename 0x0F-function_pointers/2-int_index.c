c2e9ad9d0c1bada954a1302126eec2dd8dfa5e3f
Edheramirez <edher.ramirez@outlook.es>
2021-03-11 00:13:16 -0500

    int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array && size && cmp)
-	{
 		for (i = 0; i < size; i++)
 			if (cmp(array[i])
 			    return(i);
-	}
+
 	return (-1);
 }
