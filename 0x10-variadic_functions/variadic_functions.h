aaa26bd879a5fc72ecb646179cc292a5a1cd16ae
Edheramirez <edher.ramirez@outlook.es>
2021-03-11 19:04:09 -0500

    update header and add 1-main.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,7 @@
 /* sum_them_all - function that returns the sum of all its parameters */
 int sum_them_all(const unsigned int n, ...);
 
+/* print_numbers - function that prints numbers, followed by a new line. */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif
