dce43ae7a8543e4c56ff289184a2805f789a2afc
Edheramirez <edher.ramirez@outlook.es>
2021-03-21 12:45:06 -0500

    start new proyect

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_list - function that prints all the elements of a list_t list.
@@ -9,5 +8,11 @@
 */
 size_t print_list(const list_t *h)
 {
-	
+	if (h == NULL)
+		return (0);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	return (1 + print_list(h->next));
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,24 @@
-#ifndef HOLBERTON-H
-#define HOLBERTON-H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 /* print_list - function that prints all the elements of a list_t list */
 size_t print_list(const list_t *h);
