28bc6cb296fb1bd3af8f218b98c6fac1b122c535
Edheramirez <edher.ramirez@outlook.es>
2021-03-21 19:50:24 -0500

    update header and 3main

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,4 +29,8 @@ size_t list_len(const list_t *h);
 
 /* add_node - adds a new node at the beginning of a list_t list*/
 list_t *add_node(list_t **head, const char *str);
+
+/* add_node_end - adds a new node at the end of a list_t list */
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif
