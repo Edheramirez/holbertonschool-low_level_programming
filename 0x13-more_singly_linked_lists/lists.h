446199ebe25b64b1123a4e6b489d097b3cec35b9
Edheramirez <edher.ramirez@outlook.es>
2021-03-22 18:00:51 -0500

    start new proyect add README header and main

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* print_listint - prints all the elements of a listint_t list */
+size_t print_listint(const listint_t *h);
+
+#endif
