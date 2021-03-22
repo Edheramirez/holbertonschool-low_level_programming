9a4b1cbbb4be4ef4674093cc7d2eb4fcde3ad8c4
Edheramirez <edher.ramirez@outlook.es>
2021-03-22 18:37:21 -0500

    print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: pointer to an struct.
+ *
+ * Return: number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->n == '\0')
+		printf("Error\n");
+	else
+		printf("%i\n", h->n);
+	return (1 + print_listint(h->next));
+}
