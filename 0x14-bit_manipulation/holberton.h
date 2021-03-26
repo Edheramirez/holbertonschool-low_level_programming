3ee6680c707cfe2987a9b0d37f6e18247859d4dd
Edheramirez <edher.ramirez@outlook.es>
2021-03-26 01:15:01 -0500

    start new proyect

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* binary_to_uint - converts a binary number to an unsigned int */
+unsigned int binary_to_uint(const char *b);
+
+/* print_binary - prints the binary representation of a number */
+void print_binary(unsigned long int n);
+
+/* get_bit - returns the value of a bit at a given index */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* set_bit - sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* clear_bit - sets the value of a bit to 0 at a given index */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/*flip_bits-returns the number of bits flip to get from one number to another*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+
+#endif
