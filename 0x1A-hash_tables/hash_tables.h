1b5b73502212522c26da3496288edd19112c17fd
Edheramirez <edher.ramirez@outlook.es>
2021-05-28 19:20:02 -0500

    add djb2 algorithm, add 1main test add djb2 to header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,6 +37,9 @@ typedef struct hash_table_s
 /*function that creates a hash table*/
 hash_table_t *hash_table_create(unsigned long int size);
 
+/*function implementing the djb2 algorithm*/
+unsigned long int hash_djb2(const unsigned char *str);
+
 /*function that gives you the index of a key*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
