5a1546d27be519d3055df5681b4923510cb525e6
Edheramirez <edher.ramirez@outlook.es>
2021-05-29 00:11:57 -0500

    fix 3-hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,10 +14,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+		return (0);
 	/*add asing of new element*/
 	index = key_index((unsigned char *)key, ht->size);
 	item = ht->array[index];
-
 	while (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
@@ -31,24 +32,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		item = item->next;
 	}
 	item = malloc(sizeof(hash_node_t));
-
 	if (item == NULL)
 		return (0);
-
 	item->key = strdup(key);
 	if (item->key == NULL)
 	{
 		free(item);
 		return (0);
 	}
-
 	item->key = strdup(value);
-
 	if (item->value == NULL)
 	{
 		free(item->key);
 		free(item);
 		return (0);
 	}
+	item->next = ht->array[index];
+	ht->array[index] = item;
 	return (1);
 }
