68734ef9f7cecbae7afeed1074e8d3537c6b846d
Edheramirez <edher.ramirez@outlook.es>
2021-09-04 18:00:49 -0500

    new project Search Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*function that searches for a value in an array of integers
+ using the Linear search algorithm*/
+int linear_search(int *array, size_t size, int value);
+
+#endif
