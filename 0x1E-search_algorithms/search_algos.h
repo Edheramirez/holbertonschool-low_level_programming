1333c54fa0bd66143480019657df49511db4745c
Edheramirez <edher.ramirez@outlook.es>
2021-09-04 19:03:52 -0500

    add prototype binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*function that searches for a value in an array of integers
- using the Linear search algorithm*/
+/*
+ * function that searches for a value in an array of integers
+ * using the Linear search algorithm.
+*/
 int linear_search(int *array, size_t size, int value);
+
+/*
+ * function that searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+*/
+int binary_search(int *array, size_t size, int value);
 
 #endif
